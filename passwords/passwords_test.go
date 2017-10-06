ec8d26a2492299a0031443afe2b2d1ae07e58eb2
Rafal Golarz <git@rafalgolarz.com>
2017-10-06 04:06:07 +0200

    adding benchmark test and travis conf

---
+++ passwords/passwords_test.go
@@ -0,0 +1,37 @@
+package passwords
+
+import "testing"
+
+//go test -bench=.
+func BenchmarkGenerator(b *testing.B) {
+	var defaultParams, strongParams, superStrong Setting
+	defaultParams.MinLength = 8
+	defaultParams.MinSpecialCharacters = 2
+	defaultParams.MinDigits = 2
+	defaultParams.MinLowercase = 1
+	defaultParams.MinUppercase = 1
+
+	for i := 0; i < b.N; i++ {
+		Generate(defaultParams)
+	}
+
+	strongParams.MinLength = 16
+	strongParams.MinSpecialCharacters = 4
+	strongParams.MinDigits = 4
+	strongParams.MinLowercase = 2
+	strongParams.MinUppercase = 2
+
+	for i := 0; i < b.N; i++ {
+		Generate(strongParams)
+	}
+
+	superStrong.MinLength = 255
+	superStrong.MinSpecialCharacters = 100
+	superStrong.MinDigits = 50
+	superStrong.MinLowercase = 20
+	superStrong.MinUppercase = 20
+
+	for i := 0; i < b.N; i++ {
+		Generate(superStrong)
+	}
+}
